99d10acec5764ce6f357a4dd54664d916f25b387
esoft58 - Thais Muniz <thais.tml@gmail.com>
2016-10-08 10:15:18 -0300

    UC16 - wiremock

---
+++ src/main/java/br/unicamp/bookstore/service/BuscaEnderecoService.java
@@ -0,0 +1,53 @@
+package br.unicamp.bookstore.service;
+
+import br.unicamp.bookstore.Configuration;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class BuscaEnderecoService {
+
+	private Configuration configuration;
+	
+	public String buscaEndereco(String cep) {
+		try {
+			URLConnection connection = openConnection(cep);
+			String endereco = getStatusFromResponse(connection);
+			return endereco;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private String getStatusFromResponse(URLConnection connection) throws IOException {
+		StringBuilder builder = new StringBuilder();
+		DataInputStream inStream = new DataInputStream(connection.getInputStream());
+		String inputLine;
+		while ((inputLine = inStream.readLine()) != null) {
+			builder.append(inputLine);
+		}
+		inStream.close();
+		return builder.toString();
+	}
+	
+	private URLConnection openConnection(String codigo) throws MalformedURLException, IOException {
+		URL url = new URL(configuration.getUrlEndpoint() + "/sro_bin/sroii_xml.eventos");
+		URLConnection connection = url.openConnection();
+		connection.setDoOutput(true);
+		connection.setUseCaches(false);
+		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		connection.setAllowUserInteraction(false);
+		
+		PrintStream outStream = new PrintStream(connection.getOutputStream());
+		outStream.println("usuario=ECT&senha=SRO&tipo=L&resultado=T&objetos=" + codigo);
+		outStream.close();
+		return connection;
+	}
+	
+}
