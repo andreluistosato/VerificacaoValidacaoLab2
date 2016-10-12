6a525be682f0a2bc93a323ac227fd177a7e95167
Andre Souza Bonon Borges <andre.bonon@gmail.com>
2016-10-12 15:35:01 -0300

    UC14 - Steps

--- src/main/java/br/unicamp/exemplo/CalculaEntregaPedido.java
+++ src/main/java/br/unicamp/exemplo/CalculaEntregaPedido.java
@@ -2,6 +2,12 @@ package br.unicamp.exemplo;
 
 public class CalculaEntregaPedido {
 	private String result;
+	private double peso;
+	private double largura;
+	private double altura;
+	private double comprimento;
+	private String tipoServico;
+	private int cep;
 
     public String calculaFreteTempo(double peso, double largura, double altura, double comprimento, String tipoServico, int cep) {
         result = "Teste";
