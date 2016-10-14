4dcb5cde5bccbb3068a7b009de2ba314d7afb899
abner <abner.ribeiro@tegra.me>
2016-10-14 13:26:59 -0300

    corrigindo teste

--- src/test/java/br/unicamp/bookstore/endereco/BuscaEnderecoSteps.java
+++ src/test/java/br/unicamp/bookstore/endereco/BuscaEnderecoSteps.java
@@ -1,14 +1,12 @@
 package br.unicamp.bookstore.endereco;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.containing;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -16,13 +14,9 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.http.Fault;
 
 import br.unicamp.bookstore.Configuracao;
-import br.unicamp.bookstore.model.Endereco;
 import br.unicamp.bookstore.service.BuscaEnderecoService;
-import br.unicamp.bookstore.service.ConsultaStatusService;
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
@@ -40,6 +34,8 @@ public class BuscaEnderecoSteps {
 	private BuscaEnderecoService buscaEndereco;
 
 	private String endereco;
+	
+	private Throwable throwable;
 
 	@Before
 	public void setUp() {
@@ -49,6 +45,8 @@ public class BuscaEnderecoSteps {
 		MockitoAnnotations.initMocks(this);
 		Mockito.when(configuration.getBuscarEnderecoUrl())
 				.thenReturn("http://localhost:8080/ws");
+		throwable = null;
+		endereco = null;
 	}
 
 	@After
@@ -66,13 +64,17 @@ public class BuscaEnderecoSteps {
 
 	@When("^Eu informo o CEP (\\d+)$")
 	public void eu_informo_o_CEP(String cep) throws Throwable {
-		endereco = buscaEndereco.buscar(cep);
+		try {
+			endereco = buscaEndereco.buscar(cep);
+		} catch (Exception e) {
+			throwable = e;
+		}
 	}
 
 	@Then("^O resultado deve ser o endereco \"([^\"]*)\"$")
 	public void o_resultado_deve_ser_o_endereco(String enderecoEsperado) throws Throwable {
-		enderecoEsperado = "Rua Carlos Gomes, Campinas";
 		assertEquals(enderecoEsperado, endereco);
+		assertNull(throwable);
 	}
 
 	@Given("^Eu possuo um CEP incorreto com (\\d+) digitos$")
@@ -87,18 +89,20 @@ public class BuscaEnderecoSteps {
 	@Then("^O retorno contera um valor de \"([^\"]*)\"\\.$")
 	public void o_retorno_contera_um_valor_de(String retorno) throws Throwable {
 		assertEquals(retorno, endereco);
+		assertNull(throwable);
 	}
 
 	@Given("^Eu possuo um CEP incorreto com mais de (\\d+) digitos$")
-	public void eu_possuo_um_CEP_incorreto_com_mais_de_digitos(int arg1) throws Throwable {
-		wireMockServer.stubFor(get(urlEqualTo("/ws/123456789/xml/"))
-				.willReturn(aResponse().withFault(Fault.EMPTY_RESPONSE)));
+	public void eu_possuo_um_CEP_incorreto_com_mais_de_digitos(int cepIncorreto) throws Throwable {
+		wireMockServer.stubFor(get(urlEqualTo("/ws/" + cepIncorreto + "/xml/"))
+				.willReturn(aResponse().withStatus(400)));
 
 	}
 
 	@Then("^O retorno da consulta sera um (\\d+) \\(Bad Request\\)\\.$")
 	public void o_retorno_da_consulta_sera_um_Bad_Request(String retorno) throws Throwable {
-		assertEquals(retorno, "400");
+		assertNotNull(throwable);
+		assertNull(endereco);
 	}
 
 }
